8b368ccabe2d5ae2086bec185c83f4fc3998b000
Pandada <mb692906@gmail.com>
2022-11-28 14:27:22 +0100

    Modifications to exact match using levenshtein distance

--- Candidate Generation Modules/Candidate Generation Modules/CWBD Modules/exactmatch.py
+++ Candidate Generation Modules/Candidate Generation Modules/CWBD Modules/exactmatch.py
@@ -22,10 +22,9 @@ def exactmatch(length, clue):
         # has solutions of the right length.
         d = dict(Counter(candidates))
         for c in d:
-            results.append((c, alpha / (len(candidates) + 1 - d[
-                c])))  # Generates a confidence distribution based on the number of occurrences of a certain answer.
+            results.append((c, alpha / (len(candidates) + 1 - d[c])))  # Generates a confidence distribution based on
+            # the number of occurrences of a certain answer.
     return results
-
 
 
 def a_tad_less_exactmatch(length: int, clue: str):
@@ -45,7 +44,7 @@ def a_tad_less_exactmatch(length: int, clue: str):
     for c in main.CWDB:
         r = False
         i = 0
-        while i and not r :
+        while i and not r:
             if levenshtein(c.Clue, clue) <= i:
                 candidates.append((c.Word, i))
                 r = True
@@ -53,15 +52,22 @@ def a_tad_less_exactmatch(length: int, clue: str):
 
     sorted(candidates)
     d = {}
-    for c in candidates :
-        if c[0] not in d.keys() :
-            d[c[0]] = (c[1],1)
+    for c in candidates:
+        if c[0] not in d.keys():
+            d[c[0]] = (c[1], 1)
 
-        else :
+        else:
             w, n = d[c[0]]
-            d[c[0]] = ((n*w + c[1])/(n+1), n+1)
+            d[c[0]] = ((n * w + c[1]) / (n + 1), n + 1)
 
+    co = 0
+    for c in d:
+        co += len(c) == length
 
-
-
-
+    if co != 0:
+        alpha = 1 - np.tanh((len(candidates) / co) - 1)  # Generates a confidence factor based on the number of answers
+        # to the clue over the numbers of right length. The factor is such that it tends to one when the clue mainly
+        # has solutions of the right length.
+        for c in d:
+            results.append((c, alpha / (1 + d[c][1])))  # Generates a confidence distribution based on the number of
+            # occurrences of a certain answer.
