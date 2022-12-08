ae093710c103e22f052390b0a235b7a688b011c4
Pandada <mb692906@gmail.com>
2022-12-08 08:30:20 +0100

    Modifications to partialmatchmodule.py so it doesn't divide by 0 anymore. Still doesn't give pretty results tho. Need to modify line 66's weighing function.

--- Candidate Generation Modules/IR Modules/partialmatchmodule.py
+++ Candidate Generation Modules/IR Modules/partialmatchmodule.py
@@ -1,4 +1,3 @@
-import re
 import main
 import numpy as np
 import math
@@ -8,40 +7,50 @@ vsb = [w for w in main.words]  # Creation of a basis for the Vector Space Module
 allwords = {}
 for w in vsb:
     allwords[w] = 0
+size_dict = len(vsb)
 
 freq = {}  # Representation of the vector space with a dictionary of dictionaries of the coordinates of the clue in vsb
 for c in main.CWDB:
     d = c.Clue.split(" ")  # Separation of all words in the clue in elements in vsb
     n = len(d)
-    f = {}
+    tfd = {}
     for i in d:  # Computing a dictionary of frequencies of words in the clue
-        if i not in f:
-            f[i] = [1, i]
+        if i not in tfd:
+            tfd[i] = 1
         else:
-            f[i][0] += 1
-    freq[c.Clue] = f
-    for i in f:
-        allwords[f[i][1]] += 1
+            tfd[i] += 1
+    freq[c.Clue] = tfd
+    for i in tfd:
+        allwords[i] += 1
 
+vs = {}
+for c in main.CWDB:
+    coords = {}
+    d = c.Clue.split(" ")  # Separation of all words in the clue in elements in vsb
+    n = len(d)
+    for i in freq[c.Clue]:
+        coords[i] = freq[c.Clue][i]*np.log(size_dict/allwords[i])/n  # tf-idf weight distribution
+    vs[c.Clue] = coords
 
 
 def partial_match(clue: str):
-    coords = {}
-    clue1 = clue.split(" ")  # Separation of all words in the clue in elements in vsb
-    leng = len(clue1)
-    fre = {}
-    for w in clue1:  # Computing a dictionary of frequencies of words in the clue
-        if w not in fre:
-            fre[w] = [1, w]
+    coords_c = {}
+    d_c = clue.split(" ")  # Separation of all words in the clue in elements in vsb
+    leng = len(d_c)
+    tfd_c = {}
+    for w in d_c:  # Computing a dictionary of frequencies of words in the clue
+        if w not in tfd_c:
+            tfd_c[w] = 1
         else:
-            fre[w][0] += 1
-    for w in fre:
+            tfd_c[w] += 1
+    for w in tfd_c:
         # Creation of the dictionary containing the coordinates of the clue in vsb
-        coords[fre[w][1]] = round(-np.log(fre[w][0] / leng), 3)
+        coords_c[w] = tfd_c[w]/leng * np.log(size_dict/(allwords[w] + tfd_c[w]))  # tf-idf weight distribution
 
-    def dot(co1, co2): # Given dictionaries representing the coordinates of two vectors in vsb :
-        n1 = math.sqrt(sum([pow(co1[_], 2) for _ in co1]))
-        n2 = math.sqrt(sum([pow(co2[_], 2) for _ in co2]))
+    def dot(co1, co2):  # Given dictionaries representing the coordinates of two vectors in vsb computes the dot
+        # product of those vectors
+        n1 = np.sqrt(sum([pow(co1[_], 2) for _ in co1]))
+        n2 = np.sqrt(sum([pow(co2[_], 2) for _ in co2]))
         s = 0
         for co in co1:
             if co in co2:
@@ -51,11 +60,13 @@ def partial_match(clue: str):
         return s/(n1*n2)
 
     results = []
-    for c in main.CWDB:
-        k = dot(freq[c.Clue], coords)
+    for clue in main.CWDB:
+        k = dot(vs[clue.Clue], coords_c)  # Calculates the dot product between the clue and any clue in the CWBD
         if k != 0:
-            results.append((c.Clue, k))
-
+            weight = np.sqrt(k*1/size_dict)  # Interpolates the weight between the dot product and the inverse of the
+            # number of all known words
+            results.append((clue.Clue, weight))
+    results.sort(key = lambda t: t[1], reverse = True)
     return results
 
 
