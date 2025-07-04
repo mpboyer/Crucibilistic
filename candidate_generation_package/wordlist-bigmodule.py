1d7b6e76c99ad7740a533b9b4d8e5be81c94df1b
mpboyer <matthieupierre.boyer@gmail.com>
2025-07-03 22:50:06 -0400

    clean start on main branch

--- candidate_generation_package/wordlist-bigmodule.py
+++
@@ -1,7 +0,0 @@
-def wordlist_bigmodule(length) :
-	candidates = []
-	# TODO : Find a way to combine words so that they stay acceptable
-	return candidates
-
-# This was abandoned for the presentation given the amount of time needed to make it work properly. It will be
-# implemented at later point in time.
